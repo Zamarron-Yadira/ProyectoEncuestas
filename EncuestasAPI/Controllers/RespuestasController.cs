6db8864555de79f48e1e5181d08216b0cb3864ff
YadiraZamarron21 <201G0292@rcarbonifera.tecnm.mx>
2025-05-29 23:40:27 -0600

    .

---
+++ EncuestasAPI/Controllers/RespuestasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncuestasAPI.Controllers
+{
+	[Authorize]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RespuestasController : ControllerBase
+	{
+		//AGREGAR RESPUESTA Y EDITAR, VER TODAS LAS RESPUESTAS Y por ID DE PREGUNTA
+		
+	}
+}
