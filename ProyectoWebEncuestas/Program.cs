42336133b7a03b513178f919db411c594ab31659
YadiraZamarron21 <201G0292@rcarbonifera.tecnm.mx>
2025-06-03 22:50:04 -0600

    Agregar carpetas a proyecto web y asignar usings al program

--- ProyectoWebEncuestas/Program.cs
+++ ProyectoWebEncuestas/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//	.AddCookie(options =>
+//	{
+//		options.LoginPath = "/iniciar-sesion";
+//		options.LogoutPath = "/cerrar-sesion";
+//		options.Cookie.Name = "ASGcookie";
+//		options.ExpireTimeSpan = TimeSpan.FromDays(7);
+//	});
+//builder.Services.AddAuthorization();
+//builder.Services.AddHttpClient();
 app.MapGet("/", () => "Hello World!");
 
+app.MapRazorPages();
+app.UseStaticFiles();
+app.UseDeveloperExceptionPage();
+//app.UseAuthentication();
+//app.UseAuthorization();
+app.MapControllerRoute(
+	name: "default",
+	 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+app.MapDefaultControllerRoute();
 app.Run();
